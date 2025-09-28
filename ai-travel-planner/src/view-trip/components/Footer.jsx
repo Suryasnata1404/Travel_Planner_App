162a1b2d31b0a20ca39a67589a6d575e35b0a1e1
Suryasnata1404 <suryasnata2004@gmail.com>
2025-09-29 03:05:55 +0530

    Added footer "AI Travel Planner • Crafted with ❤️ by Suryasnata"

---
+++ ai-travel-planner/src/view-trip/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='mt-10 mb-5'>
+       <h2 className="text-center text-gray-500 font-semibold">
+         AI Travel Planner • <span className="font-normal italic">Crafted with ❤️ by Suryasnata</span>
+       </h2>
+
+    </div>
+  )
+}
+
+export default Footer
