1f3ff2dda15cd5d27feece8764bf49e4adb43751
Suryasnata1404 <suryasnata2004@gmail.com>
2025-09-28 18:25:32 +0530

    ViewTrip-InfoSection

--- ai-travel-planner/src/view-trip/[tripId]/index.jsx
+++ ai-travel-planner/src/view-trip/[tripId]/index.jsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import { toast } from 'sonner';
+import InfoSection from '../components/InfoSection';
 
 function Viewtrip() {
   const {tripId}=useParams();
@@ -32,8 +33,17 @@ function Viewtrip() {
   }
 
   return (
-    <div>
+    <div className='p-10 md:px-20 lg:px-44 xl:px-56'>
+
+      {/* Information Section */}
+      <InfoSection trip={trip} />
+
+      {/* Recommended Hotels */}
+
+      {/* Daily Plan */}
       
+      {/* Footer */}
+     
     </div>
   )
 }
---
+++ ai-travel-planner/src/view-trip/components/InfoSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function InfoSection({trip}) {
+  return (
+    <div>
+        <img src='/placeholder.jpg' className='h-[340px] w-full object-cover rounded-xl' />
+
+        <div>
+            <h2></h2>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default InfoSection
