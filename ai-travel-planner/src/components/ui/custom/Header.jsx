7f86259a545795076d66857da5a362204942e384
Suryasnata1404 <suryasnata2004@gmail.com>
2025-09-30 02:10:07 +0530

    Update SignIn Logout

--- ai-travel-planner/src/components/ui/custom/Header.jsx
+++ ai-travel-planner/src/components/ui/custom/Header.jsx
@@ -1,29 +1,122 @@
-import React, { useEffect } from 'react'
-import { Button } from '../button'
+import React, { useEffect, useState } from "react";
+import { Button } from "../button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import { toast } from "sonner";
+import axios from "axios";
+import { FcGoogle } from "react-icons/fc";
 
 function Header() {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [openDialog, setOpenDialog] = useState(false);
+  
 
-  useEffect(()=>{
-    console.log(user)
-  },[])
+  useEffect(() => {
+    console.log(user);
+  }, []);
 
+  const login= useGoogleLogin({
+    onSuccess:(Response)=>GetUserProfile(Response),
+    onError:(error)=> {
+      toast.error("Google login failed. Please try again.");
+      console.error(error);
+    }
+  })
+
+  const GetUserProfile = (tokenInfo) => {
+    axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${tokenInfo?.access_token}`,{
+      headers:{
+        Authorization:`Bearer ${tokenInfo?.access_token}`,
+        Accept:`Application/json`
+
+      }
+    }).then((resp) =>{
+      console.log(resp);
+      localStorage.setItem('user', JSON.stringify(resp.data));
+      setOpenDialog(false);
+      window.location.reload();
+    }).catch((error) => {
+      console.error("Error fetching user profile: ", error);
+    });
+  }
 
   return (
-    <div className='p-1 shadow-sm flex justify-between items-center px-5'>
-        <img src='/logo.svg' width="60" height= "70" />
-        <div>
-          {user ?
-            <div className='flex items-center gap-3'>
-              <Button variant="outline" className="rounded-full">My Trips</Button>
-              <img src={user?.picture} className='h-[35px] w-[35px] rounded-full'/>
-            </div>
-            :
-            <Button>Sign In</Button>
-          } 
-        </div>
+    <div className="p-1 shadow-sm flex justify-between items-center px-5">
+      <img src="/logo.svg" width="60" height="70" />
+      <div>
+        {user ? (
+          <div className="flex items-center gap-3">
+            <Button variant="outline" className="rounded-full">
+              My Trips
+            </Button>
+
+            <Popover>
+              <PopoverTrigger>
+                <img
+                  src={user?.picture}
+                  className="h-[35px] w-[35px] rounded-full"
+                />
+              </PopoverTrigger>
+              <PopoverContent>
+                <h2
+                  className="cursor-pointer"
+                  onClick={() => {
+                    googleLogout();
+                    localStorage.clear();
+                    window.location.reload();
+                  }}
+                >
+                  Logout
+                </h2>
+              </PopoverContent>
+            </Popover>
+          </div>
+        ) : (
+          <Button onClick={()=>setOpenDialog(true)}>Sign In</Button>
+        )}
+      </div>
+
+
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+              <img
+                src="/logo.svg"
+                alt="logo"
+                width="65px"
+                className="items-center"
+              />
+              <h2 className="font-bold text-lg">
+                Sign In to check out your travel plan
+              </h2>
+              <p>Sign in to the App with Google authentication securely</p>
+
+              <Button
+                onClick={login}
+                className="w-full mt-5 flex gap-4 items-center"
+              >
+                <FcGoogle className="h-7 w-15" />
+                Sign In With Google
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
