e3144fdcba3fc4e506e2ab86bf35f9a2c68cbc83
Suryasnata1404 <suryasnata2004@gmail.com>
2025-10-03 01:51:56 +0530

    Update

--- ai-travel-planner/src/view-trip/components/Hotels.jsx
+++ ai-travel-planner/src/view-trip/components/Hotels.jsx
@@ -8,7 +8,7 @@ function Hotels({ trip }) {
 
       <div className="grid grid-cols-2 md:grid-cols-3 xl-grid-cols-4 gap-5">
         {trip?.tripData?.hotel_options?.map((hotel, index) => (
-            <HotelCardItem hotel={hotel} />
+            <HotelCardItem hotel={hotel}  key={hotel.id || index}/>
         ))}
       </div>
     </div>
