e5bd47693e81bc5a453766a97322b342a1e9fa77
Suryasnata1404 <suryasnata2004@gmail.com>
2025-10-14 00:50:59 +0530

    update

--- ai-travel-planner/src/components/ui/custom/Hero.jsx
+++ ai-travel-planner/src/components/ui/custom/Hero.jsx
@@ -34,10 +34,12 @@ function Hero() {
         
         {/* <img src="/landing.png" alt="" className='w-[750px]' /> */}
 
+
+
          {/* 🖼️ Marquee Animation Section */}
-      <div className="relative flex flex-col w-[80vw] md:w-[75vw] overflow-hidden mt-10">
-        {/* Top row — moves left */}
-        <MarqueeWrapper speed={50}>
+      <div className="relative flex flex-col w-[90vw] md:w-[75vw] justify-center overflow-hidden mt-10">
+        {/* Top row — moves right */}
+        <MarqueeWrapper pauseOnHover className="[--duration:50s]">
           {firstHalf.map((img, i) => (
             <img
               key={i}
@@ -51,7 +53,7 @@ function Hero() {
         </MarqueeWrapper>
 
         {/* Bottom row — moves right */}
-        <MarqueeWrapper reverse speed={50}>
+        <MarqueeWrapper reverse pauseOnHover className="[--duration:50s]">
           {secondHalf.map((img, i) => (
             <img
               key={i}
@@ -69,7 +71,6 @@ function Hero() {
         <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white"></div>
         <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white"></div>
       </div>
-
       
     </div>
   )
