97a54cf0ea971ccf05798c90a94e22896c918d05
Suryasnata1404 <suryasnata2004@gmail.com>
2025-10-14 00:51:39 +0530

    update

--- ai-travel-planner/src/components/ui/Marquee.jsx
+++ ai-travel-planner/src/components/ui/Marquee.jsx
@@ -1,23 +1,44 @@
-// /src/components/ui/Marquee.jsx
+import { cn } from "@/lib/utils";
 import React from "react";
-import Marquee from "react-fast-marquee";
+//import Marquee from "react-fast-marquee";
+
 
 export default function MarqueeWrapper({
-  children,
+  className ,
   reverse = false,
-  speed = 40,
-  pauseOnHover = true,
-  className = "",
+  pauseOnHover = false,
+  children,
+  vertical = false,
+  repeat = 4,
+  ...props
 }) {
   return (
-    <Marquee
-      direction={reverse ? "left" : "right"}
-      pauseOnHover={pauseOnHover}
-      speed={speed}
-      gradient={false}
-      className={className}
+    <div
+      {...props}
+      className={cn(
+        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)]",
+        {
+          "flex-row": !vertical,
+          "flex-col": vertical,
+        },
+        className
+      )}
     >
-      {children}
-    </Marquee>
+      {Array(repeat)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={cn("flex shrink-0 justify-around [gap:var(--gap)]", {
+              "animate-marquee flex-row": !vertical,
+              "animate-marquee-vertical flex-col": vertical,
+              "group-hover:[animation-play-state:paused]": pauseOnHover,
+              "[animation-direction:reverse]": reverse,
+            })}
+          >
+            {children}
+          </div>
+        ))}
+    </div>
   );
 }
