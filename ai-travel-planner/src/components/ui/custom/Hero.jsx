a553c6236eadc9e1a1cc9299274efb86d28da292
Suryasnata Mohapatra <suryasnata2004@gmail.com>
2025-10-20 23:58:26 +0530

    Clean up Hero.jsx by removing blank lines
    Removed unnecessary blank lines for cleaner code.

--- ai-travel-planner/src/components/ui/custom/Hero.jsx
+++ ai-travel-planner/src/components/ui/custom/Hero.jsx
@@ -33,8 +33,6 @@ function Hero() {
         
         {/* <img src="/landing.png" alt="" className='w-[750px]' /> */}
 
-
-
          {/* 🖼️ Marquee Animation Section */}
       <div className="relative flex flex-col w-[90vw] md:w-[75vw] justify-center overflow-hidden mt-10">
         {/* Top row — moves right */}
@@ -64,7 +62,6 @@ function Hero() {
             />
           ))}
         </MarqueeWrapper>
-
 
         {/* Gradient edges for smooth fade */}
         <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white"></div>
