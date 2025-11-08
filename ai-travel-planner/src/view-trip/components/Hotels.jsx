50555a53a172672dbc497de21cb0dfba5b323b0b
Suryasnata1404 <suryasnata2004@gmail.com>
2025-11-09 02:08:09 +0530

    Update

--- ai-travel-planner/src/view-trip/components/Hotels.jsx
+++ ai-travel-planner/src/view-trip/components/Hotels.jsx
@@ -4,7 +4,11 @@ import HotelCardItem from "./HotelCardItem";
 function Hotels({ trip }) {
   return (
     <div>
-      <h2 className="font-bold text-xl mt-5">Hotel Recommendation</h2>
+      
+        <h2 className="font-extrabold text-3xl text-center bg-gradient-to-r from-[#17aded] to-[#074751] bg-clip-text text-transparent mb-6 relative">
+            Hotel Recommendation
+            <span className="block mx-auto mt-2 h-[3px] w-16 bg-[#073041] rounded-full"></span>
+        </h2>
 
       <div className="grid grid-cols-2 md:grid-cols-3 xl-grid-cols-4 gap-5">
         {trip?.tripData?.hotel_options?.map((hotel, index) => (
