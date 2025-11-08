31e55b657ce6b6ac6bbb185bb16b04953bf29b70
Suryasnata1404 <suryasnata2004@gmail.com>
2025-11-09 00:43:32 +0530

    Updated

--- ai-travel-planner/src/components/ui/custom/Footer.jsx
+++ ai-travel-planner/src/components/ui/custom/Footer.jsx
@@ -40,12 +40,10 @@ function Footer({ footerRef }) {
   return (
     <footer
       ref={footerRef}
-      className="w-full flex flex-col items-center justify-center border-t border-gray-200
-                 bg-gradient-to-t from-[#e0f7ff] to-white text-gray-700 py-6"
+      className="w-full flex flex-col items-center justify-center
+                 bg-gradient-to-t from-[#7ad6f4] to-white text-gray-700 py-6"
     >
-      <p className="font-semibold text-lg bg-gradient-to-b from-[#0ba5e9] to-[#0f8fa0] bg-clip-text text-transparent">
-        GoTravel © 2025
-      </p>
+      <p className="font-semibold text-lg bg-gradient-to-b from-[#0ba5e9] to-[#0f8fa0] bg-clip-text text-transparent"> GoTravel © 2025 </p>
 
       <div className="flex items-center justify-center gap-6 mt-3 text-2xl text-[#0f8fa0]">
         {socialIcons.map((item, index) => (
@@ -54,16 +52,16 @@ function Footer({ footerRef }) {
             to={item.link}
             target="_blank"
             rel="noopener noreferrer"
-            className="transition-transform duration-300 hover:scale-125 hover:text-[#0ba5e9]"
+            className="transition-transform duration-300 hover:scale-125 hover:text-[#0da6e8]"
           >
             {item.icon}
           </Link>
         ))}
       </div>
 
-      <p className="text-xs mt-4 text-gray-500 text-center max-w-md">
-        Built with ❤️ using React, Tailwind, and AI ✈️  
-        <br /> Let’s make your next adventure effortless.
+      <p className="text-s mt-4 text-gray-500 text-center max-w-md">
+        Crafted with ❤️ and wanderlust ✈️ 
+        <br /> Let’s make your next trip a story worth telling!
       </p>
     </footer>
   );
